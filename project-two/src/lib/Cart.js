7fff51df83b23c183b638d0d548f9627a9bb1b42
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-05 19:54:06 -0300

    add tests to get total and add product

--- project-two/src/lib/Cart.js
+++ project-two/src/lib/Cart.js
@@ -1,5 +1,16 @@
-const sum = (n1, n2) => {
-  n1 + n2;
-};
+export class Cart {
+  constructor() {
+    this.itens = [];
+  }
 
-export default sum;
+  getTotal() {
+    return this.itens.reduce((acc, { product, quantity }) => {
+      acc += product.price * quantity;
+      return acc;
+    }, 0);
+  }
+
+  add(item) {
+    this.itens.push(item);
+  }
+}
--- project-two/src/lib/Cart.spec.js
+++ project-two/src/lib/Cart.spec.js
@@ -1,3 +1,25 @@
-it('sum 2 and 2 must result in 4', () => {
-  expect(2 + 2).toBe(4);
+import { Cart } from './Cart';
+
+describe('Cart', () => {
+  let cart = new Cart();
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
+  it('should return 0 when getTotal() is executed in a newly created instance', () => {
+    expect(cart.getTotal()).toEqual(0);
+  });
+
+  it('should multipy quantity and price and return the total amount', () => {
+    const item = {
+      product: {
+        title: 'Airpods Pro',
+        price: 35388,
+      },
+      quantity: 2,
+    };
+
+    cart.add(item);
+    expect(cart.getTotal()).toEqual(70776);
+  });
 });
